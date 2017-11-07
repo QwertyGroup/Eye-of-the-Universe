481b93e8542885fba219d4ad72f257cb982c1ef1
Arseniy Kucher <beeffallo@gmail.com>
2017-11-07 22:36:08 +0200

    Echo

--- EyeOfTheUniverse.py
+++ EyeOfTheUniverse.py
@@ -1,4 +1,28 @@
-import math
-for i in range(15):
-    r = math.pow(i, 3)
-    print(r)
+import telegram
+import time
+from telegram.ext import *
+
+TACCKEN = '390252714:AAE0YvbmlmPOkyPp-JbmW33ujEOuL8qOgAw'
+
+bot = telegram.Bot(token=TACCKEN)
+print(f'{bot.get_me().first_name} was inited!')
+updater = Updater(token=TACCKEN)
+dispatcher = updater.dispatcher
+
+
+def OnStart(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+
+def Echo(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
+
+
+dispatcher.add_handler(CommandHandler('start', OnStart))
+dispatcher.add_handler(MessageHandler(Filters.text, Echo))
+
+updater.start_polling()
+
+time.sleep(30)
+print('finished.')
+updater.stop()
