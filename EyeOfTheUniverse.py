6c4f4465cc6f543ba84e95c9b87a1d139976ba12
Arseniy Kucher <beeffallo@gmail.com>
2017-12-22 15:03:00 +0200

    Starting up

--- EyeOfTheUniverse.py
+++ EyeOfTheUniverse.py
@@ -1,7 +1,11 @@
 import telegram
+from telegram.ext import *
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+from emoji import emojize
+
 import time
 import os
-from telegram.ext import *
+
 
 TACCKEN = '390252714:AAE0YvbmlmPOkyPp-JbmW33ujEOuL8qOgAw'
 
@@ -11,61 +15,10 @@ updater = Updater(token=TACCKEN)
 dispatcher = updater.dispatcher
 
 
-def OnStart(bot, update):
+def OnStartCmd(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
 
-def Echo(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
-    bot.send_message(chat_id=update.message.chat_id, text=update.message.audio)
-    print(update.message.text)
-    print(update.message)
-
-
-def AudioRepl(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Music")
-    print('Auido file received')
-
-
-def VoiceRepl(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="VoiceRepl")
-    print('VoiceRepl file received')
-    ans = f'{update.message.voice.duration}, {update.message.voice.mime_type}'
-    print(ans)
-    bot.send_message(chat_id=update.message.chat_id, text=ans)
-
-
-def OnTestMessage(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="OnTestMessageStart")
-    directory = os.path.dirname('tmp/')
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    with open('tmp/file.txt', 'w') as fi:
-        fi.write('which is always equal to the length of the string')
-
-    with open('tmp/file.txt') as fi:
-        lines = fi.readlines()
-        print(lines)
-        bot.send_message(chat_id=update.message.chat_id, text=lines)
-
-    with open('TESTFILE_T.txt', 'w') as fi:
-        fi.write("My test text!")
-    time.sleep(3)
-    os.remove('tmp/file.txt')
-    os.rmdir('tmp/')
-    os.remove('TESTFILE_T.txt')
-    print('deleted.')
-    dlist = os.listdir('.')
-    print(os.getcwd())
-    bot.send_message(chat_id=update.message.chat_id, text=os.getcwd())
-    bot.send_message(chat_id=update.message.chat_id, text=[str(i) for i in dlist])
-
-
-dispatcher.add_handler(CommandHandler('start', OnStart))
-dispatcher.add_handler(CommandHandler('test!', OnTestMessage))
-
-dispatcher.add_handler(MessageHandler(Filters.text, Echo))
-dispatcher.add_handler(MessageHandler(Filters.audio, AudioRepl))
-dispatcher.add_handler(MessageHandler(Filters.voice, VoiceRepl))
+dispatcher.add_handler(CommandHandler('start', OnStartCmd))
 
 updater.start_polling()
